7af606cd9e19ee760e7209327442b9861c9117ea
joyislam1954 <joyislam1954@gmail.com>
2021-03-26 21:53:34 +0600

    ETender

--- ETender/government_employee/models.py
+++ ETender/government_employee/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
+from SuperAdmin.models import Ministry
 
 
 class Holder(models.Model):
@@ -9,3 +10,15 @@ class Holder(models.Model):
 
     def __str__(self):
         return self.lisence_number
+
+
+class TenderUpload(models.Model):
+    username = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ministry_name = models.ForeignKey(Ministry, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150, verbose_name='Tender Title')
+    publish_date = models.DateField(auto_now_add=True, verbose_name='Tender Publish Date')
+    end_date = models.DateField(verbose_name='Tender Publish Date End')
+    pdf = models.FileField(verbose_name='Tender Details Pdf')
+
+    def __str__(self):
+        return self.title
